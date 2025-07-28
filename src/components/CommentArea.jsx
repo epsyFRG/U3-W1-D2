88d46fd0e87ae848e20b1040f36ab63230b0e66f
Emiliano Massari <158219820+epsyFRG@users.noreply.github.com>
2025-07-28 14:14:58 +0200

    U3-W2-D1

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { Component } from "react"
 import Loading from "./Loading"
 import Error from "./Error"
 import CommentsList from "./CommentsList"
@@ -7,53 +7,82 @@ import AddComment from "./AddComment"
 const AUTH_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODgyMWQ3NGZlMzZkMDAwMTU5NzU4MTIiLCJpYXQiOjE3NTMzNTc2ODQsImV4cCI6MTc1NDU2NzI4NH0.qetkzLPaCgXs3j8p2CpTb4s4TBdPVIdXrBq1n4Hm6WY"
 
-const CommentArea = ({ book }) => {
-  const [comments, setComments] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+class CommentArea extends Component {
+  state = {
+    comments: [],
+    loading: false,
+    error: null,
+  }
 
-  useEffect(() => {
-    const fetchComments = async () => {
-      setLoading(true)
-      setError(null)
-      try {
-        const res = await fetch(
-          `https://striveschool-api.herokuapp.com/api/comments/${book.asin}`,
-          {
-            headers: {
-              Authorization: `Bearer ${AUTH_TOKEN}`,
-            },
-          }
-        )
-        if (!res.ok) throw new Error("Errore nel recupero dei commenti")
-        const data = await res.json()
-        setComments(data)
-      } catch (err) {
-        setError(err.message)
-      } finally {
-        setLoading(false)
-      }
+  componentDidMount() {
+    if (this.props.asin) {
+      this.fetchComments(this.props.asin)
     }
-    fetchComments()
-  }, [book.asin])
-
-  const handleCommentAdded = (newComment) => {
-    setComments((prev) => [...prev, newComment])
   }
 
-  const handleCommentDeleted = (commentId) => {
-    setComments((prev) => prev.filter((c) => c._id !== commentId))
+  componentDidUpdate(prevProps) {
+    if (this.props.asin && this.props.asin !== prevProps.asin) {
+      this.fetchComments(this.props.asin)
+    }
   }
 
-  if (loading) return <Loading />
-  if (error) return <Error message={error} />
+  fetchComments = async (asin) => {
+    this.setState({ loading: true, error: null })
+    try {
+      const res = await fetch(
+        `https://striveschool-api.herokuapp.com/api/comments/${asin}`,
+        {
+          headers: {
+            Authorization: `Bearer ${AUTH_TOKEN}`,
+          },
+        }
+      )
+      if (!res.ok) throw new Error("Errore nel recupero dei commenti")
+      const data = await res.json()
+      this.setState({ comments: data })
+    } catch (err) {
+      this.setState({ error: err.message })
+    } finally {
+      this.setState({ loading: false })
+    }
+  }
 
-  return (
-    <div className="mt-2">
-      <CommentsList comments={comments} onDelete={handleCommentDeleted} />
-      <AddComment asin={book.asin} onCommentAdded={handleCommentAdded} />
-    </div>
-  )
+  handleCommentAdded = (newComment) => {
+    this.setState((prevState) => ({
+      comments: [...prevState.comments, newComment],
+    }))
+  }
+
+  handleCommentDeleted = (commentId) => {
+    this.setState((prevState) => ({
+      comments: prevState.comments.filter((c) => c._id !== commentId),
+    }))
+  }
+
+  render() {
+    const { comments, loading, error } = this.state
+    const { asin } = this.props
+
+    if (!asin) {
+      return (
+        <div className="alert alert-info mt-4">
+          Seleziona un libro per vedere i commenti
+        </div>
+      )
+    }
+    if (loading) return <Loading />
+    if (error) return <Error message={error} />
+
+    return (
+      <div className="mt-2">
+        <CommentsList
+          comments={comments}
+          onDelete={this.handleCommentDeleted}
+        />
+        <AddComment asin={asin} onCommentAdded={this.handleCommentAdded} />
+      </div>
+    )
+  }
 }
 
 export default CommentArea
