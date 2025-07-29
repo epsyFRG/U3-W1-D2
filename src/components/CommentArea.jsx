83587ad3a7b88c8128cea4ed9ba25ebbad321c37
Emiliano Massari <158219820+epsyFRG@users.noreply.github.com>
2025-07-29 13:30:43 +0200

    U3-W2-D2

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
 import Loading from "./Loading"
 import Error from "./Error"
 import CommentsList from "./CommentsList"
@@ -7,27 +7,21 @@ import AddComment from "./AddComment"
 const AUTH_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODgyMWQ3NGZlMzZkMDAwMTU5NzU4MTIiLCJpYXQiOjE3NTMzNTc2ODQsImV4cCI6MTc1NDU2NzI4NH0.qetkzLPaCgXs3j8p2CpTb4s4TBdPVIdXrBq1n4Hm6WY"
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-    loading: false,
-    error: null,
-  }
+function CommentArea({ asin }) {
+  const [comments, setComments] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  componentDidMount() {
-    if (this.props.asin) {
-      this.fetchComments(this.props.asin)
+  useEffect(() => {
+    if (asin) {
+      fetchComments(asin)
     }
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [asin])
 
-  componentDidUpdate(prevProps) {
-    if (this.props.asin && this.props.asin !== prevProps.asin) {
-      this.fetchComments(this.props.asin)
-    }
-  }
-
-  fetchComments = async (asin) => {
-    this.setState({ loading: true, error: null })
+  const fetchComments = async (asin) => {
+    setLoading(true)
+    setError(null)
     try {
       const res = await fetch(
         `https://striveschool-api.herokuapp.com/api/comments/${asin}`,
@@ -39,50 +33,40 @@ class CommentArea extends Component {
       )
       if (!res.ok) throw new Error("Errore nel recupero dei commenti")
       const data = await res.json()
-      this.setState({ comments: data })
+      setComments(data)
     } catch (err) {
-      this.setState({ error: err.message })
+      setError(err.message)
     } finally {
-      this.setState({ loading: false })
+      setLoading(false)
     }
   }
 
-  handleCommentAdded = (newComment) => {
-    this.setState((prevState) => ({
-      comments: [...prevState.comments, newComment],
-    }))
+  const handleCommentAdded = (newComment) => {
+    setComments((prevComments) => [...prevComments, newComment])
   }
 
-  handleCommentDeleted = (commentId) => {
-    this.setState((prevState) => ({
-      comments: prevState.comments.filter((c) => c._id !== commentId),
-    }))
+  const handleCommentDeleted = (commentId) => {
+    setComments((prevComments) =>
+      prevComments.filter((c) => c._id !== commentId)
+    )
   }
 
-  render() {
-    const { comments, loading, error } = this.state
-    const { asin } = this.props
-
-    if (!asin) {
-      return (
-        <div className="alert alert-info mt-4">
-          Seleziona un libro per vedere i commenti
-        </div>
-      )
-    }
-    if (loading) return <Loading />
-    if (error) return <Error message={error} />
-
+  if (!asin) {
     return (
-      <div className="mt-2">
-        <CommentsList
-          comments={comments}
-          onDelete={this.handleCommentDeleted}
-        />
-        <AddComment asin={asin} onCommentAdded={this.handleCommentAdded} />
+      <div className="alert alert-info mt-4">
+        Seleziona un libro per vedere i commenti
       </div>
     )
   }
+  if (loading) return <Loading />
+  if (error) return <Error message={error} />
+
+  return (
+    <div className="mt-2">
+      <CommentsList comments={comments} onDelete={handleCommentDeleted} />
+      <AddComment asin={asin} onCommentAdded={handleCommentAdded} />
+    </div>
+  )
 }
 
 export default CommentArea
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,26 +1,23 @@
-import React, { Component } from "react"
+import React from "react"
 import { Card } from "react-bootstrap"
 
-class SingleBook extends Component {
-  render() {
-    const { book, selectedAsin } = this.props
-    const isSelected = selectedAsin === book.asin
-    return (
-      <Card
-        style={{
-          border: isSelected ? "2px solid red" : "1px solid #ddd",
-          cursor: "pointer",
-        }}
-        className="h-100"
-      >
-        <Card.Img variant="top" src={book.img} alt={book.title} />
-        <Card.Body>
-          <Card.Title>{book.title}</Card.Title>
-          <Card.Text>{book.category}</Card.Text>
-        </Card.Body>
-      </Card>
-    )
-  }
+function SingleBook({ book, selectedAsin }) {
+  const isSelected = selectedAsin === book.asin
+  return (
+    <Card
+      style={{
+        border: isSelected ? "2px solid red" : "1px solid #ddd",
+        cursor: "pointer",
+      }}
+      className="h-100"
+    >
+      <Card.Img variant="top" src={book.img} alt={book.title} />
+      <Card.Body>
+        <Card.Title>{book.title}</Card.Title>
+        <Card.Text>{book.category}</Card.Text>
+      </Card.Body>
+    </Card>
+  )
 }
 
 export default SingleBook
